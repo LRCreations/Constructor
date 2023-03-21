602825ed225dd66ee79bae62dae64338883fe770
LRCreations <lr20company21@gmail.com>
2023-03-21 22:15:49 +0530

    Java Constructor Project Code

---
+++ src/Company.java
@@ -0,0 +1,12 @@
+import javax.sound.midi.Soundbank;
+
+public class Company {
+    String brandName;
+    Company() {
+        brandName = "Apple";
+    }
+    public static void main(String[] args) {
+        Company c = new Company();
+        System.out.println(c.brandName);
+    }
+}
